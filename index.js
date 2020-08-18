8c460f4214768e3fba4a0410995e7c638c8bb99b
willytan-96 <willy.tan434@gmail.com>
2020-08-18 13:20:54 +0700

    Add Express

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+// Bot Code
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const axios =  require('axios');
