b86cc0d70806bee4e4352105022e24d4cd329f24
Willy Tan <willy.tan434@gmail.com>
2020-11-18 20:00:23 +0700

    Update description items

--- src/features/equipment-filters/index.js
+++ src/features/equipment-filters/index.js
@@ -8,18 +8,50 @@ async function synthesisEquipment(message) {
   const searchMessage = message.content.split(EQUIPMENT_CONSTANT.SYNTHESIS);
   const searchItemName = searchMessage[1].toLowerCase();
 
-  const tierList = [
-    { tier: "", value: 0},
-    { tier: "I", value: 1},
-    { tier: "II", value: 2},
-    { tier: "III", value: 3},
-    { tier: "IV", value: 4},
-    { tier: "V", value: 5},
-    { tier: "VI", value: 6},
-    { tier: "VII", value: 7},
-    { tier: "VIII", value: 8},
-    { tier: "IX", value: 9},
-    { tier: "X", value: 10}
+  const tierList = [{
+      tier: "",
+      value: 0
+    },
+    {
+      tier: "I",
+      value: 1
+    },
+    {
+      tier: "II",
+      value: 2
+    },
+    {
+      tier: "III",
+      value: 3
+    },
+    {
+      tier: "IV",
+      value: 4
+    },
+    {
+      tier: "V",
+      value: 5
+    },
+    {
+      tier: "VI",
+      value: 6
+    },
+    {
+      tier: "VII",
+      value: 7
+    },
+    {
+      tier: "VIII",
+      value: 8
+    },
+    {
+      tier: "IX",
+      value: 9
+    },
+    {
+      tier: "X",
+      value: 10
+    }
   ]
 
   if (searchItemName) {
@@ -38,58 +70,99 @@ async function synthesisEquipment(message) {
 
     let maxScore = 0;
     let result = [];
-  
+
     equipments.forEach((equipment) => {
       const currentItemName = equipment.itemName.toLowerCase();
       let score = stringSimilarity.compareTwoStrings(currentItemName, searchItemName);
-  
+
       if (maxScore < score) {
         maxScore = score;
         result = equipment;
       }
     });
-  
+
     if (maxScore === 0) message.channel.send('Equipment is not found');
     else {
       await axios.get(`https://www.romcodex.com/api/item/${result.itemId}`)
-        .then((response) => {
-          const weaponDetails = response.data;
-
+        .then(async ({
+          data: weaponDetails
+        }) => {
           if (weaponDetails.SynthesisRecipe.length > 0) {
             const synthesisRecipe = weaponDetails.SynthesisRecipe[0];
 
-            var synthesisMaterials = ""; 
-            synthesisRecipe.input.materials.forEach((material) => {
-              synthesisMaterials +=  `${material.quantity}x **` + material.item.NameZh__EN + '**\n';
+            var synthesisMaterials = "";
+            synthesisRecipe.input.materials.forEach(({ quantity, item }) => {
+              synthesisMaterials += `${quantity}x **` + item.NameZh__EN + '**\n';
             })
 
-            var synthesisEquipments = ""; 
+            var synthesisEquipments = "";
             synthesisRecipe.input.weapons.forEach((weapon) => {
               const tiertext = tierList.filter((tier) => tier.value === weapon.tier)[0];
               synthesisEquipments += "1x **" + weapon.item.NameZh__EN + ` ${tiertext.tier}**\n`
             })
 
+            const { id: syntesisOutputId } = synthesisRecipe.output;
+
+            var synthesisWeaponDetails = weaponDetails;
+            if (synthesisWeaponDetails.id !== syntesisOutputId) {
+              await axios.get(`https://www.romcodex.com/api/item/${syntesisOutputId}`)
+                .then(({
+                  data: synthesisDetails
+                }) => {
+                  synthesisWeaponDetails = synthesisDetails;
+                }).catch((error) => {
+                  console.log(error);
+                  message.channel.send("Can't retrieve syntesis weapon detail information");
+                })
+            }
+
+            let synthesisWeaponStatus = "";
+            let synthesisWeaponExtraStatus = "";
+
+            const {
+              Stat: synthesisWeaponDetailStats,
+              StatExtra: synthesisWeaponDetailExtraStats
+            } = synthesisWeaponDetails.AttrData;
+
+            synthesisWeaponDetailStats.forEach((status, index) => {
+              synthesisWeaponStatus += `- ${status}${index+1 === synthesisWeaponDetailStats.length ? "" : "\n"}`
+            })
+
+            synthesisWeaponDetailExtraStats.forEach((status, index) => {
+              synthesisWeaponExtraStatus += `- ${status}${index+1 === synthesisWeaponDetailExtraStats.length ? "" : "\n"}`
+            })
+
+            const synthesisWeaponItemName = synthesisWeaponDetails.NameZh__EN || "Unknown";
+            const synthesisWeaponThumbnail = `https://www.romcodex.com/icons/item/item_${syntesisOutputId}.png`;
+            const synthesisWeaponItemType = synthesisWeaponDetails.TypeName;
+            const synthesisWeaponPrice = `${synthesisRecipe.cost || "Unknown"} [Never Updated]`;
+
             const exampleEmbed = new Discord.RichEmbed()
               .setColor('#0099ff')
-              .setTitle(synthesisRecipe.output.item.NameZh__EN || "???")
-              .setThumbnail(`https://www.romcodex.com/icons/item/item_${synthesisRecipe.output.id}.png`)
-              .addField("Materials", synthesisMaterials)
+              .setTitle(synthesisWeaponItemName)
+              .setThumbnail(synthesisWeaponThumbnail)
+              .addField("Item Type", synthesisWeaponItemType)
+              .addField("Status", synthesisWeaponStatus)
+              .addField("Effects", synthesisWeaponExtraStatus)
+              .addBlankField()  
+              .addField("Upgrade Materials:", "---------------------", false)
+              .addField("Monster Materials", synthesisMaterials)
               .addField("Weapon Materials", synthesisEquipments)
-              .addField('Cost Price', synthesisRecipe.cost || "???")
-              .setTimestamp()
+              .addField('Cost Price', synthesisWeaponPrice)
 
             message.channel.send(exampleEmbed);
           }
         })
         .catch((error) => {
-          message.channel.send("Can't fetch data details from server, kalau bukan TULIS NAMANYA YANG BENER !!")
+          console.log(error)
+          message.channel.send("Failed to fetch data from server.\nPlease input the correct name.")
         })
     }
   } else {
-    message.channel.send("Mana nama itemnya jir!!")
+    message.channel.send("Please input the item name.")
   }
 
-  
+
 }
 
 module.exports = {
